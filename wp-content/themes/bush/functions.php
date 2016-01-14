601cd152b2d7bc5b26cbd3eaf6e0a8fb5e25ef27
Robert M. Parker <rparker@yamiko.ninja>
2016-01-14 12:03:36 -0800

    add css and js to theme

--- wp-content/themes/bush/functions.php
+++ wp-content/themes/bush/functions.php
@@ -4,8 +4,20 @@ require_once 'vendor/autoload.php';
 use Bush\App;
 use Bush\WordPress\Menu;
 use Bush\WordPress\StyleSheet;
+use Bush\WordPress\Script;
 
-$app = new App();
+// stylesheets
+$stylesheet_app = new StyleSheet('bush_app_css', StyleSheet::getThemeURL() . '/stylesheets/app.css');
 
-$stylesheet = new StyleSheet('bush', StyleSheet::getThemeURL() . '/stylesheets/app.css');
-$menu_1 = new Menu('location', 'and a description');
+// scripts
+add_action('wp_enqueue_scripts', function () {
+    wp_deregister_script('jquery');
+});
+$script_jquery = new Script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js');
+$script_foundation = new Script('foundation', Script::getThemeURL() . '/bower_components/foundation-sites/dist/foundation.min.js');
+$script_app = new Script('bush_app_js', Script::getThemeURL() . '/js/app.min.js', [
+    'foundation'
+], time());
+
+// menus
+$menu_primary = new Menu('primary', 'primary menu used in header');
