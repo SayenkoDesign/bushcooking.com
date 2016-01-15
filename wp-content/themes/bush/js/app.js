acc4379deb638009b47563710ca0ce2d50949f67
Robert M. Parker <rparker@yamiko.ninja>
2016-01-15 00:43:38 -0800

    add classes to wordpress mobile nav for foundation

--- wp-content/themes/bush/js/app.js
+++ wp-content/themes/bush/js/app.js
@@ -6,5 +6,14 @@ jQuery(function() {
         'class': 'submenu menu vertical',
         'data-submenu': true
     });
+
+    $('#primary-mobile-nav').attr({
+        'class': 'vertical menu hide-for-medium',
+        'data-accordion-menu': true
+    }).find('ul').attr({
+        'class': 'menu vertical nested',
+        'data-submenu': true
+    });
+    
     jQuery(document).foundation();
 });
