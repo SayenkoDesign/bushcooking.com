6f5809ea5504687db616234646bb8200af7f5253
Robert M. Parker <rparker@yamiko.ninja>
2016-01-29 04:42:13 -0800

    refactored templates

--- wp-content/themes/bush/comments.php
+++ wp-content/themes/bush/comments.php
@@ -3,13 +3,23 @@ global $app;
 $comments = get_comments([
 	'post_id' => get_the_ID(),
 ]);
+
+$content = [];
 foreach($comments as $comment){
-	echo $app->render('template/comment.html.twig', [
+	$content[] = $app->render('partials/comment.html.twig', [
 			'comment' => $comment,
 			'rating' => get_comment_meta($comment->comment_ID, 'rating', true)
 	]);
 }
+
+ob_start();
 comment_form([
 	'title_reply' => 'Leave a Comment',
 	'label_submit' => "Leave Comment",
+]);
+$form = ob_get_clean();
+
+echo $app->render('template/comments.html.twig', [
+	'comments' => $content,
+	'comment_form' => $form,
 ]);
