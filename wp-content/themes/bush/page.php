de13498c3fe6d1db543ab9e9aff361d49cf638de
diaboloshogunate <rparker@kodama.design>
2017-06-14 13:19:13 -0700

    only equlize on two columns and fix nothing found heading displaying when there is content

--- wp-content/themes/bush/page.php
+++ wp-content/themes/bush/page.php
@@ -4,6 +4,11 @@ global $app;
 
 while (have_posts()) {
     the_post();
-    echo $app->render('pages/page.html.twig');
+    ob_start();
+    the_content();
+    $content = ob_get_clean();
+    echo $app->render('pages/page.html.twig', [
+        'content' => $content,
+    ]);
 }
 get_footer();
